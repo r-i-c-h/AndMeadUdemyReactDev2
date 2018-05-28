a28461eb208db9b228cd6a974cec1fc9816bb772
r-i-c-h <rich@richwerden.com>
2018-05-27 21:31:58 -0700

    Begins Implementing React-Router

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-/* import XXXXXX from './components/XXXXXX'; */
-
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<h1>PUT YOUR APP HERE</h1>, document.getElementById('app'));
+const ExpenseDashboardPage = () => ( <div>Expense Dashboard Page</div> );
+const AddExpensePage = () => ( <div>Add Expense Page</div> );
+const EditExpensePage = () => ( <div>EDIT Expense Page</div> );
+const HelpPage = () => ( <div>HAAALLLP!</div> );
+const NotFoundPage = () => ( <div>404!!!!</div> );
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
