64df3e2fd7c3cb8d80b5c0fce5f7d0877ddfef80
r-i-c-h <rich@richwerden.com>
2018-06-06 00:08:02 -0700

    Configures Adding New Expense Process flow

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import ExpenseForm from './ExpenseForm';
+import { connect } from 'react-redux';
 
-const AddExpensePage = () => (
+import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses'; // wants Description, Note, Amount, CreatedAt
+
+const AddExpensePage = (props) => (
   <div>
     <h1>Add Expense</h1>
-    <ExpenseForm />
+    <ExpenseForm 
+      onSubmit={ (expense) => {
+        props.dispatch(addExpense(expense));
+        props.history.push('/'); // history is from from react-router
+      }}
+    
+    />
   </div>
 ); 
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);   
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,59 +1,72 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-
 import { SingleDatePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
 
-// const now = moment();
-// console.log(now.format('MMM Do, YYYY h:mA'));
-
+// Doesn't actually hit a reducer because the same component works for both ADD and EDIT 
+// - instead /those/ pass down their own versions of props.onSubmit() 
 export default class ExpenseForm extends Component {
   state = {
-    description: '',
-    note: '',
     amount: '',
     createdAt: moment(),
-    datePickerFocused: false
-  };
-
-  handleDescriptionChange = (e) => {
-    const description = e.target.value;
-    this.setState( ()=> ({description}) );
+    description: '',
+    note: '',
+    datePickerFocused: false,
+    error: ''
   };
   /** Alternate Style of onChange Handling: Instead of starting with a var defined as e.target.value **/
   /** You CAN access the e.target.value directly, BUT you MUST call e.persist() first! **/
   /* handleFooChange = (e) => { e.persist(); this.setState(()=>{ foo: e.target.value}); } */
+  handleDescriptionChange = (e) => {
+    const description = e.target.value;
+    this.setState( ()=> ({description}) );
+  };
   handleNoteTextChange = (e) => {
     const note = e.target.value;
     this.setState( ()=> ({note}) );
   };
   handleAmountChange = (e) => {
     const amount = e.target.value;
-    if ( amount.match(/^\d*(\.\d{0,2})?$/)) {
+    if ( !amount || amount.match(/^\d+(\.\d{0,2})?$/) ) {
       this.setState( ()=> ({amount}) );
     }
   };
-  handleDateChange = (createdAt) => {
-    // per docs, this gets called with a new moment time obj as the arg...
-    this.setState( ()=> ({createdAt}) );
+  handleDateChange = (createdAt) => { // per docs, this gets called with a new moment.js time Obj{} as the arg...
+    if (createdAt) {  // This 'if' statement ensures the field is never empty\cleared by user...
+      this.setState( ()=> ({createdAt}) );
+    }
   };
-  onFocusChange = ({ focused }) => {
+  onFocusChange = ({ focused }) => { // <--req'd by Air-bnb date picker component
     this.setState( ()=> ({ datePickerFocused: focused }) );
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    if ( !this.state.description || !this.state.amount ) {
+      this.setState( () => ({ error: 'Description+Amount are REQUIRED in order to Submit!' }) );
+    } else {
+      this.setState( () => ({ error: '' }) );
+      this.props.onSubmit({ // Call the passed-in Props version so we can reuse this ExpenseForm with either ADD or EDIT
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      });
+    }
   };
 
   render() {
     return (
       <div>
-        <form>
-          <input 
-            type="text"
+      { this.state.error && <p>{this.state.error}</p> }
+        <form onSubmit={this.onSubmit}>
+          <input type="text"
             placeholder="Description"
-            autoFocus
             value={this.state.description}
             onChange={this.handleDescriptionChange}
+            autoFocus
           />
-          $<input
-            type="text"
+          $<input type="text"
             placeholder="Amount"
             value={this.state.amount}
             onChange={this.handleAmountChange}
@@ -64,13 +77,12 @@ export default class ExpenseForm extends Component {
             focused={this.state.datePickerFocused}
             onFocusChange={this.onFocusChange}
             numberOfMonths={1}
-            isOutsideRange={ () => { false } }
+            isOutsideRange={()=>{false}} 
           />
           <textarea
             placeholder="Note to add..."
             onChange={this.handleNoteTextChange}
-          >
-          </textarea>
+          ></textarea>
           <button>Add Expense</button>
         </form>
       </div>
