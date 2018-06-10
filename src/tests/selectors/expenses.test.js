53b21b5660c700d465251dcef4f06c489fc3fe05
r-i-c-h <rich@richwerden.com>
2018-06-09 17:22:31 -0700

    Breaks out dummy data

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,29 +1,6 @@
 import moment from 'moment';
 import selectExpenses from '../../selectors/expenses';
-
-const dummyData = [
-  {
-    id:'1',
-    description:'thing-1',
-    note:'lorem ipsum 1',
-    amount: 111,
-    createdAt: 0
-  },
-  {
-    id:'2',
-    description:'the second',
-    note:'',
-    amount: 222,
-    createdAt: moment(0).subtract(4, 'days').valueOf()
-  },
-  {
-    id:'3',
-    description:'los third-olio',
-    note:'threeer',
-    amount: 333,
-    createdAt: moment(0).add(4, 'days').valueOf()
-  },
-];
+import dummyData from '../FIXTURES/dummyExpensesDataArray';
 
 test('should filter by text value',()=>{
   const filterObj = {
