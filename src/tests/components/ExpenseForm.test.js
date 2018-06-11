f559f4e312b4e81264467b2c913bd9c9052bdb09
r-i-c-h <rich@richwerden.com>
2018-06-10 19:11:53 -0700

    Adds some artificial onChange/Click tests for EventForm

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -12,3 +12,43 @@ test('should render ExpenseForm with Data aok', () => {
   const wrapper = shallow(<ExpenseForm expense={FAKE[0]} />);
   expect(wrapper).toMatchSnapshot();
 });
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+test('should render error for invalid submission', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+
+  wrapper.find('form').simulate('submit', {
+    preventDefault:()=>{}
+  });
+  expect(wrapper.state('error').length).toBeGreaterThan(0);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should set description on input change', () => {
+  const value = 'Lorem Ipsum';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find( 'input' ).at(0).simulate( 'change', {target:{ value }}); // <~~ at() is akin to query_[0] or nth-child 
+  expect(wrapper.state('description')).toBe(value);
+}); 
+
+test('should set note on textbox change', () => {
+  const value = 'Lorem Ipsum';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find( 'textarea' ).simulate( 'change', {target:{ value }}); // 
+  expect(wrapper.state('note')).toBe(value);
+}); 
+
+test('should setState on valid input', () => {
+  const value = '123.45';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find( 'input' ).at(1).simulate( 'change', {target:{ value }});
+  expect(wrapper.state('amount')).toBe(value);
+});
+
+test('should NOT setState on invalid input', () => {
+    const value = '0123.321';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find( 'input' ).at(1).simulate( 'change', {target:{ value }});
+    expect(wrapper.state('amount')).toBe('');
+});
+
