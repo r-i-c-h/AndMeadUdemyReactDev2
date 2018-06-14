50db03c544308892eb986622fdf0c62882aa0618
r-i-c-h <rich@richwerden.com>
2018-06-13 19:21:45 -0700

    Setup webpack for production Build

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,35 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.jsx?$/,
-      exclude: /node_modules/
+module.exports = (env) => {
+  const isProductionBuild = env === 'production' ? 'production' : false;
+// using `webpack --env.development` will make  env = {development:true}
+// just testing for production here...
+  return {
+    mode:  isProductionBuild ? 'production': 'development',
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
     },
-    {
-      test: /\.s?css$/,
-      use:[
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.jsx?$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        use:[
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+    },
+    devtool: isProductionBuild ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
 };
