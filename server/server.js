c8dccc102a3fbe00d2a8fb9302d6b0856684b6f6
r-i-c-h <rich@richwerden.com>
2018-06-25 03:05:18 -0700

    starts Heroku Dynamic Builds

--- server/server.js
+++ server/server.js
@@ -2,11 +2,14 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 const publicDir = path.join(__dirname, '..', 'public');
 
 app.use( express.static(publicDir) );
 
 app.get('*', (req,res) => {
-  res.sendFile( path.join(publicDir,'index.html') );
+  res.sendFile( path.join(publicDir, 'index.html'));
 });
-app.listen(3000,()=>{ console.log('server up on port 3000');});
+
+app.listen(port,()=>{ console.log(`server up on port ${port}`);});
